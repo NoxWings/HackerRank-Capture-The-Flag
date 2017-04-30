573a84614d045416439f3f686141cb4d842a4362
David G. Miguel <noxwings@gmail.com>
2017-04-30 22:57:42 +0200

    Create infinite_links.py
    Added infinite links solution (problem 3) using python3 with "requests" library and "lxml" parser

---
+++ infinite_links.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import requests
+from lxml import html
+
+base_url = 'https://cdn.hackerrank.com/hackerrank/static/contests/capture-the-flag/infinite/'
+
+remaining = [base_url + 'qds.html']
+seen = set()
+passwds = set()
+
+while remaining:
+    next_url = remaining.pop()
+    print('Requesting: '.format(next_url))
+    dom = html.fromstring(requests.get(next_url).text)
+    dom.make_links_absolute(base_url)
+    seen.add(next_url)
+
+    passwd = dom.xpath('//font/text()')
+    if passwd:
+        passwds.add(passwd[0])
+
+    for link in dom.xpath('//a/@href'):
+        if link not in seen:
+            remaining.append(link)
+
+print("#" * 80)
+for passwd in sorted(passwds):
+    print(passwd)
