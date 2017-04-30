71b8996ccdd4a578d72f56a1926a1194d302c093
David G. Miguel <noxwings@gmail.com>
2017-04-30 22:55:03 +0200

    Create secret_key.py
    Added secret key challenge solution using python3 and requests library

---
+++ secret_key.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import requests
+import json
+
+keys_web = 'https://cdn.hackerrank.com/hackerrank/static/contests/capture-the-flag/secret/key.json'
+detail_web = 'https://cdn.hackerrank.com/hackerrank/static/contests/capture-the-flag/secret/secret_json/{}.json'
+keys = json.loads(requests.get(keys_web).text)
+news = [json.loads(requests.get(detail_web.format(key)).text)['news_title'] for key in keys]
+for n in news:
+    print(n)
